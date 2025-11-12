8d3cc30bb845154a6453732cc3ce441d86154ef5
jaasiel790 <amirezjaasiel@gmail.com>
2025-11-12 20:48:50 +0000

    finished html and started working on java

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Script loaded");
+
+// function for history button
+display();
+
+function display() {
+    console.log("Display function called");
+    let display = document.getElementById("display");
+    let numbers = document.getElementById("numbers");
+    let buttons = document.getElementById("buttons");
+    //show numbers and buttons
+    
+
+    return display;
+}
+
+
+function showHistory() {
+   console.log("History button clicked");
+   let history = [0]
+   for(let i = 0; i < localStorage.length; i++) {
+    
+   }
+}
+
+function showResult() {
+    console.log("Show result function called");
+    //call result and store it in a variable
+    let result = document.getElementById("display");
+    
+    //
+}
+
